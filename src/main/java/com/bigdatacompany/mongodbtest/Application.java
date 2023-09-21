d1bfa7332a7dc713d9d27aeed8b2947e7b566de4
Furkan Civan <furkancivan37@gmail.com>
2023-09-22 00:46:05 +0300

    Adding more than one data to collection.

--- src/main/java/com/bigdatacompany/mongodbtest/Application.java
+++ src/main/java/com/bigdatacompany/mongodbtest/Application.java
@@ -8,6 +8,8 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 
+import java.util.Arrays;
+
 public class Application {
     public static void main(String[] args) {
         ConnectionString connectionString = new ConnectionString("mongodb://localhost:27017");
@@ -19,6 +21,13 @@ public class Application {
                 .append("date","2000")
                 .append("country","Türkiye");
 
-        personalCollection.insertOne(Document.parse(data.toJson()));
+        BasicDBObject data2 = new BasicDBObject().append("name", "Mahmut")
+                .append("date","1993")
+                .append("country","Türkiye");
+
+        Document parse = Document.parse(data.toJson());
+        Document parse2 = Document.parse(data2.toJson());
+
+        personalCollection.insertMany(Arrays.asList(parse,parse2));
     }
 }
